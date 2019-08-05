5abcafc95abb0839c5b5afa9d5e419d734777de6
BETH-zhang <522718277@qq.com>
2019-08-05 12:10:55 +0800

    添加game案例入口

---
+++ src/components/Draw/game.stories.js
@@ -0,0 +1,8 @@
+// http://www.sohu.com/a/165261084_283613
+import React from 'react';
+import { storiesOf } from '@storybook/react'
+import Component from './index'
+
+storiesOf('Game|Demo1', module)
+  .add('默认', () => <Component render={(ctx, uc, canvas) => {
+  }} />)
